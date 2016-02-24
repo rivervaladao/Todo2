976d4cb355d662486a3d79bbc4099a4328b907cf
rivervaladao <rivervaladao@hotmail.com>
2016-02-24 11:11:07 -0300

    adicionado fragment_list_view

---
+++ app/src/main/java/com/river/app/todo/ListViewFragment.java
@@ -0,0 +1,137 @@
+package com.river.app.todo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.river.app.todo.model.DBData;
+import com.river.app.todo.model.Tarefa;
+
+public class ListViewFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private StaggeredGridLayoutManager staggeredGridLayoutManager;
+    private TodoAdapter mAdapter;
+    Menu mMenu;
+    boolean isListView;
+
+    public ListViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            final Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_view, container, false);
+
+        //apresentar
+        setHasOptionsMenu(true);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list);
+
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(1,
+                StaggeredGridLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(staggeredGridLayoutManager);
+        mAdapter = new TodoAdapter(getContext());
+
+        TodoAdapter.OnItemClick onItemClickListener = new TodoAdapter.OnItemClick() {
+
+            @Override
+            public void onItemClick(View view, int position) {
+
+                Tarefa tarefa = DBData.tarefaList().get(position);
+
+                TodoFragmentDetail detailFragment =
+                        TodoFragmentDetail.newInstance(tarefa.getCategoria().toString(),
+                                tarefa.getDecricao());
+
+                if (savedInstanceState == null) {
+
+
+                    FragmentManager fm = getFragmentManager();
+                    fm.beginTransaction()
+                            .replace(R.id.main_layout, detailFragment, "todoDetailFragment")
+                            .addToBackStack(null)
+                            .commit();
+                }
+
+            }
+        };
+
+        mAdapter.setOnItemClickListener(onItemClickListener);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "FAB button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        isListView = true;
+
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.menu_main,menu);
+        mMenu = menu;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_toggle:
+                toggle();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void toggle(){
+        MenuItem item = mMenu.findItem(R.id.action_toggle);
+        if(isListView){
+            staggeredGridLayoutManager.setSpanCount(2);
+            item.setIcon(R.drawable.ic_action_list);
+            item.setTitle("Ver Lista");
+            isListView=false;
+        }else{
+            staggeredGridLayoutManager.setSpanCount(1);
+            item.setIcon(R.drawable.ic_action_grid);
+            item.setTitle("Ver Grid");
+            isListView=true;
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
--- app/src/main/java/com/river/app/todo/MainActivity.java
+++ app/src/main/java/com/river/app/todo/MainActivity.java
@@ -2,11 +2,15 @@ package com.river.app.todo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,10 +18,10 @@ import com.river.app.todo.model.DBData;
 import com.river.app.todo.model.Tarefa;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private StaggeredGridLayoutManager staggeredGridLayoutManager;
-    private TodoAdapter mAdapter;
+
     Toolbar toolbar;
+
+
     private void setUpActionBar() {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -35,49 +39,15 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setUpActionBar();
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        ListViewFragment listViewFragment = new ListViewFragment();
 
-        staggeredGridLayoutManager = new StaggeredGridLayoutManager(1,
-                StaggeredGridLayoutManager.VERTICAL);
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.main_layout,listViewFragment,"list_view_fragment_key")
+            .commit();
 
-        mRecyclerView.setLayoutManager(staggeredGridLayoutManager);
-        mAdapter = new TodoAdapter(this);
-
-        TodoAdapter.OnItemClick onItemClickListener = new TodoAdapter.OnItemClick() {
-
-             @Override
-            public void onItemClick(View view, int position) {
-
-                 Tarefa tarefa = DBData.tarefaList().get(position);
-
-                 TodoFragmentDetail detailFragment =
-                         TodoFragmentDetail.newInstance(tarefa.getCategoria().toString(),
-                                 tarefa.getDecricao());
-
-                if (savedInstanceState == null) {
-
-
-                    FragmentManager fm = getSupportFragmentManager();
-                    fm.beginTransaction()
-                            .replace(R.id.main_layout, detailFragment, "todoDetailFragment")
-                            .addToBackStack(null)
-                            .commit();
-                }
-
-            }
-        };
-
-        mAdapter.setOnItemClickListener(onItemClickListener);
-
-        mRecyclerView.setAdapter(mAdapter);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"FAB button clicked", Toast.LENGTH_LONG).show();
-            }
-        });
-
+        }
     }
+
 }
